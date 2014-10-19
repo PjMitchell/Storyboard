ec0ae5274833a37a3be7ff8b4c441eab3ad9404f
PjMitchell <peterjames.m@gmail.com>
2014-10-19 16:36:00 +0100

    adding missing addStoryController

---
+++ Storyboard.Web/App/Home/Controllers/AddStoryController.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path="../homemodule.ts" />
+/// <reference path="../../../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../models/addupdatestorycommand.ts" />
+
+module Home {
+    export class AddStoryController {
+        static $inject = ['$location','StoryOverviewDataService'];
+        private dataService: Home.StoryOverviewDataService;
+        private location: ng.ILocationService;         
+        constructor($location: ng.ILocationService, StoryOverviewDataService :Home.StoryOverviewDataService) {
+            this.dataService = StoryOverviewDataService;
+            this.location = $location;
+            this.Command = new Home.AddUpdateStoryCommand();
+            this.IsSaving = false;
+        }
+        Command: Home.AddUpdateStoryCommand
+        IsSaving: boolean
+        add(){
+            this.IsSaving = true;
+            this.dataService.add(this.Command).success(this.onSave)
+        }
+        
+        private onSave = () => {
+            this.location.path('/')
+
+        }
+    }
+}
