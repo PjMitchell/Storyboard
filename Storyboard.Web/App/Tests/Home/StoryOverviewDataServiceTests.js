244db667fe6d3d3f92a0c60cb142ef088fc41fbd
PjMitchell <peter@hilltop-digital.co.uk>
2014-10-25 22:08:01 +0100

    Added Jasmine and Tests for StoryOverviewDataServiceTests

---
+++ Storyboard.Web/App/Tests/Home/StoryOverviewDataServiceTests.js
@@ -0,0 +1,45 @@
+﻿/// <reference path="../../../scripts/typings/jasmine/jasmine.d.ts" />
+/// <reference path="../../home/services/storyoverviewdataservice.ts" />
+/// <reference path="../../../scripts/typings/angularjs/angular.d.ts" />
+describe('StoryOverviewDataService', function () {
+    var fakeHttpService;
+    var apiRoute;
+    var service;
+
+    beforeEach(function () {
+        fakeHttpService = {};
+        fakeHttpService.get = function (input) {
+            return {};
+        };
+        fakeHttpService.post = function (input, data) {
+            return {};
+        };
+        fakeHttpService.delete = function (input) {
+            return {};
+        };
+        apiRoute = '/api/StoryOverview';
+        service = new Home.StoryOverviewDataService(fakeHttpService);
+    });
+
+    it('Gets calls StoryOverview api', function () {
+        spyOn(fakeHttpService, 'get');
+        service.getAll();
+        expect(fakeHttpService.get).toHaveBeenCalledWith(apiRoute);
+    });
+
+    it('Add call StoryOverview api', function () {
+        spyOn(fakeHttpService, 'post');
+        var command = new Home.AddUpdateStoryCommand();
+        command.Id = 1;
+        service.add(command);
+        expect(fakeHttpService.post).toHaveBeenCalledWith(apiRoute, command);
+    });
+
+    it('Delete call StoryOverview api', function () {
+        spyOn(fakeHttpService, 'delete');
+        var id = 1;
+        service.delete(id);
+        expect(fakeHttpService.delete).toHaveBeenCalledWith(apiRoute + '/' + id);
+    });
+});
+//# sourceMappingURL=StoryOverviewDataServiceTests.js.map
---
+++ Storyboard.Web/App/Tests/Home/StoryOverviewDataServiceTests.ts
@@ -0,0 +1,49 @@
+﻿/// <reference path="../../../scripts/typings/jasmine/jasmine.d.ts" />
+/// <reference path="../../home/services/storyoverviewdataservice.ts" />
+/// <reference path="../../../scripts/typings/angularjs/angular.d.ts" />
+
+
+describe('StoryOverviewDataService', () => {
+    var fakeHttpService: ng.IHttpService;
+    var apiRoute: string;
+    var service: Home.IStoryOverviewDataService;
+
+    beforeEach(() =>
+    {
+        fakeHttpService = <ng.IHttpService>{};
+        fakeHttpService.get = (input: string) => { return <ng.IHttpPromise<any>>{} };
+        fakeHttpService.post = (input: string, data: any) => { return <ng.IHttpPromise<any>>{} };
+        fakeHttpService.delete = (input: string) => { return <ng.IHttpPromise<any>>{}  };
+        apiRoute = '/api/StoryOverview';
+        service = new Home.StoryOverviewDataService(fakeHttpService);
+    });
+
+    it('Gets calls StoryOverview api', () => {
+        
+        
+        spyOn(fakeHttpService, 'get');
+        service.getAll();
+        expect(fakeHttpService.get).toHaveBeenCalledWith(apiRoute);
+    })
+
+    it('Add call StoryOverview api', () => {
+        
+        spyOn(fakeHttpService, 'post');
+        var command = new Home.AddUpdateStoryCommand()
+        command.Id = 1;
+        service.add(command);
+        expect(fakeHttpService.post).toHaveBeenCalledWith(apiRoute, command);
+    })
+
+    it('Delete call StoryOverview api', () => {
+
+        spyOn(fakeHttpService, 'delete');
+        var id = 1;
+        service.delete(id);
+        expect(fakeHttpService.delete).toHaveBeenCalledWith(apiRoute+'/'+id);
+    })
+
+})
+
+
+
