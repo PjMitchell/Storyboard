37973a6a34423e473233121256c163bbe4192448
PjMitchell <peterjames.m@gmail.com>
2014-12-30 13:34:00 +0000

    Added Link Controller

---
+++ Storyboard.Web.Tests/Apis/LinkControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Storyboard.Domain.Data;
+using Telerik.JustMock;
+using Storyboard.Web.API;
+using Storyboard.Domain.Core.Commands;
+
+namespace Storyboard.Web.Tests.Apis
+{
+    [TestClass]
+    public class LinkControllerTests
+    {
+        private ILinkDataService dataService;
+        private LinkController target;
+
+        [TestInitialize]
+        public void Init()
+        {
+            dataService = Mock.Create<ILinkDataService>();
+            target = new LinkController(dataService);
+
+        }
+        [TestMethod]
+        public void PostCallsLinkDataService()
+        {
+            var createCommand = new CreateLinkCommand();
+            Mock.Arrange(() => dataService.Add(createCommand)).Occurs(1);
+            target.Post(createCommand);
+            Mock.Assert(dataService);
+        }
+    }
+}
---
+++ Storyboard.Web/API/LinkController.cs
@@ -0,0 +1,52 @@
+﻿using Storyboard.Domain.Core.Commands;
+using Storyboard.Domain.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Storyboard.Web.API
+{
+    /// <summary>
+    /// Link Crud
+    /// </summary>
+    public class LinkController : ApiController
+    {
+        private ILinkDataService dataService;
+        
+        public LinkController(ILinkDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+        
+        // GET: api/Link
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET: api/Link/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST: api/Link
+        public void Post([FromBody]CreateLinkCommand command)
+        {
+            dataService.Add(command);
+        }
+
+        // PUT: api/Link/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        // DELETE: api/Link/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
