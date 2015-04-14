91b823679e237bf521ccbba46ffc91e75c5837de
PjMitchell <peter@hilltop-digital.co.uk>
2015-04-14 22:07:31 +0100

    Added StorySectionRepository

---
+++ Storyboard.Domain/Data/IStorySectionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Storyboard.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HDLink;
+
+namespace Storyboard.Domain.Data
+{
+    public interface IStorySectionRepository : IAsyncNodeRepository<StorySection>
+    {
+        OrderedHierarchicalTree<StorySection> GetTreeForStory(int storyId);
+    }
+}
