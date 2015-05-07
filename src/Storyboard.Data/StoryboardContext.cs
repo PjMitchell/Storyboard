8fc192808542796efbed1ebd3d514ce9452cbac0
PjMitchell <peter@hilltop-digital.co.uk>
2015-05-07 22:24:39 +0100

    Added Changes not captured by intital commit

---
+++ src/Storyboard.Data/StoryboardContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.Entity;
+using Storyboard.Data.DbObject;
+
+namespace Storyboard.Data
+{
+    public class StoryboardContext : DbContext
+    {
+        public StoryboardContext()
+        {
+
+        }
+
+        public DbSet<ActorTableRow> Actor { get; set; }
+        public DbSet<StoryTableRow> Story { get; set; }
+        public DbSet<LinkTableRow> Link { get; set; }
+        public DbSet<StorySectionTableRow> StorySection { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ActorTableRow>().ForSqlServer().Table("Actor", DbSchemas.Story);
+            modelBuilder.Entity<ActorTableRow>().Key(r=> r.Id);
+
+            modelBuilder.Entity<StoryTableRow>().ForSqlServer().Table("Story", DbSchemas.Story);
+            modelBuilder.Entity<StoryTableRow>().Key(r => r.Id);
+
+            modelBuilder.Entity<LinkTableRow>().ForSqlServer().Table("Link", DbSchemas.Story);
+            modelBuilder.Entity<LinkTableRow>().Key(r => r.Id);
+
+            modelBuilder.Entity<StorySectionTableRow>().ForSqlServer().Table("StorySection", DbSchemas.Story); ;
+            modelBuilder.Entity<StorySectionTableRow>().Key(r=> r.Id);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectsV12;Initial Catalog=StoryBoard;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False"); //todo remove this abomination
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ src/Storyboard.Web/gulpfile.js
@@ -0,0 +1,62 @@
+﻿/// <binding AfterBuild='sass' Clean='clean' />
+
+var gulp = require("gulp"),
+  rimraf = require("rimraf"),
+  sass = require("gulp-sass"),
+  fs = require("fs"),
+  tsc = require("gulp-typescript"),
+  concat = require("gulp-concat"),
+  flatten = require('gulp-flatten');
+
+eval("var project = " + fs.readFileSync("./project.json"));
+
+var paths = {
+  bower: "./bower_components/",
+  lib: "./" + project.webroot + "/lib/",
+  css: "./" + project.webroot + "/css/",
+  app: "./" + project.webroot + "/app/"
+};
+
+var tsOption = {
+    sortOutput: true,
+    target: 'ES5'
+}
+
+gulp.task("clean", function (cb) {
+  rimraf(paths.lib, cb);
+});
+
+gulp.task('sass', function(){
+    gulp.src('./sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('typeScript', function () {
+    gulp.src('./Scripts/AppBootstrapper.ts')
+    .pipe(tsc().js).pipe(gulp.dest(paths.app));
+    gulp.src(['./Scripts/Home/**/*.ts'])   
+    .pipe(tsc().js)
+    .pipe(flatten())
+    .pipe(concat('home.js'))
+    .pipe(gulp.dest(paths.app));
+})
+
+gulp.task("copy", ["clean"], function () {
+  var bower = {
+    "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,eot}",
+    "bootstrap-touch-carousel": "bootstrap-touch-carousel/dist/**/*.{js,css}",
+    "hammer.js": "hammer.js/hammer*.{js,map}",
+    "jquery": "jquery/jquery*.{js,map}",
+    "jquery-validation": "jquery-validation/jquery.validate.js",
+    "jquery-validation-unobtrusive": "jquery-validation-unobtrusive/jquery.validate.unobtrusive.js",
+    "angular": "angular/angular.*{js,map}",
+    "angular-route": "angular-route/angular-route.*{js,map}",
+    "angular-bootstrap": "angular-bootstrap/*.{js,map,css}"
+  }
+
+  for (var destinationDir in bower) {
+    gulp.src(paths.bower + bower[destinationDir])
+      .pipe(gulp.dest(paths.lib + destinationDir));
+  }
+});
