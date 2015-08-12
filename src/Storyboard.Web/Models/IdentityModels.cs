d5b9e17c82c73ef0a671e3cf2bd4c8243cc0fef0
PjMitchell <peter@hilltop-digital.co.uk>
2015-08-12 12:30:59 +0100

    Fixed taghelpers on Account login form

--- src/Storyboard.Web/Models/IdentityModels.cs
+++ src/Storyboard.Web/Models/IdentityModels.cs
@@ -6,7 +6,7 @@ namespace Storyboard.Web.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-    }
+            }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
