e0c107c0f04a2788d08931444fbbcac91417d81b
PjMitchell <peter@hilltop-digital.co.uk>
2015-08-12 13:41:12 +0100

    Wired in Header Component

---
+++ src/Storyboard.Web/Models/Components/HeaderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace Storyboard.Web.Models
+{
+    public class HeaderViewModel
+    {
+        public string Username { get; set; }
+        public bool IsLoggedIn { get; set; }
+    }
+}
