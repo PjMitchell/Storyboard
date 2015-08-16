25d09178d085a07ea2c013866c1d863c3125a333
PjMitchell <peterjames.m@gmail.com>
2015-08-16 08:42:58 +0100

    Added asp identity tables to db project. added using statements to account pages

---
+++ data/Storyboard.Database/Tables/dbo/AspNetUserClaims.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[AspNetUserClaims]
+(
+	[Id] INT Identity(1,1) PRIMARY KEY,
+	[ClaimType] nvarchar(max),
+	[ClaimValue] nvarchar(max),
+	[UserId] nvarchar(450),
+	CONSTRAINT [FK_UserClaim_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers](Id) 
+
+)
---
+++ data/Storyboard.Database/Tables/dbo/AspNetUserRoles.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AspNetUserRoles]
+(
+	[UserId] nvarchar(450),
+	[RoleId] nvarchar(450),
+	CONSTRAINT [FK_UserRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[AspNetRoles](Id), 
+	Constraint [PK_UserRoles] PRIMARY KEY ([UserId],[RoleId]),
+	CONSTRAINT [FK_UserRoles_UserId] FOREIGN KEY([UserId]) REFERENCES  [dbo].[AspNetUsers](Id) 
+)
