002b75d199b78ad45e3bbe3a06168432b397f585
PjMitchell <peterjames.m@gmail.com>
2015-12-28 12:49:13 +0000

    Added excluded files

---
+++ src/Storyboard.Web/App/Home/Services/OverviewDataService.ts
@@ -0,0 +1,21 @@
+﻿import {Injectable, Inject} from 'angular2/core';
+import * as models from '../models/storymodels';
+import {IAddUpdateStoryCommand} from '../models/addupdatestorycommand';
+import {HttpAdaptor, IHttpAdaptor} from '../../core/httpadaptor';
+
+export interface IOverviewDataService {
+    getStoryOverview(id: number): Promise<models.IStoryOverview>;
+}
+@Injectable()
+export class OverviewDataService implements IOverviewDataService {
+    private apiRoute = '/api/Overview';
+
+    constructor(@Inject(HttpAdaptor) private http: IHttpAdaptor) {
+    }
+
+    public getStoryOverview(id: number) {
+        return this.http.get<models.IStoryOverview>(this.apiRoute + '/' + id)
+    }
+  
+};
+ 
